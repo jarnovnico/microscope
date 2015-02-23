3104aceced38203b9713a8dc0be5cc08bdb4ec2f
jarnovnico <j.verhoogt@gmail.com>
2015-02-23 12:50:07 +0100

    ...

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	},
-	{
-		title: 'COME ON YOU SPURS',
-		url: 'http://reddit.com/r/coys'
-	}
-
-];
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
